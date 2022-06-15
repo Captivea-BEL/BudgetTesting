e6a8ec43b16a307542cba750c991290c141dac80
Bassim Elsamaloty <99155821+Captivea-BEL@users.noreply.github.com>
2022-06-15 14:08:49 -0400

    test

--- buget_burdening/models/purchase_order.py
+++ buget_burdening/models/purchase_order.py
@@ -17,7 +17,8 @@
 #
 ########################################################################################
 
-from odoo import models, fields
+from odoo import models, fields, api
+import datetime
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
@@ -41,6 +42,43 @@ class PurchaseOrder(models.Model):
 
     date_promised = fields.Date(string='Promise Date', required=True, copy=True)
 
+    @api.onchange('date_promised','price_total','account_analytic_id')
+    def onchange_budget_burden(self):
+        if self.date_promised and self.price_total and self.account_analytic_id:
+            min_time = datetime.min.time()
+            po_date = datetime.combine(self.date_promised, min_time)
+            budget = self.env['crossovered.budget.lines'].search([
+                ('analytic_account_id','=', self.analytic_account_id.id),
+                ('date_from', '>=', po_date ),
+                ('date_to', '<=', po_date )
+            ])
+            if budget:
+                po_lines = self.env['purchase.order.line'].search([
+                    ('account_analytic_id','=', budget.analytic_account_id.id),                    
+                    ('date_promised', '>=', budget.date_from ),
+                    ('date_promised', '<=', budget.date_to )
+                ])
+
+
+
+
+    #     for rec in self: 
+    #         start_time = datetime.min.time()
+    #         start_date = datetime.combine(rec.date_from, start_time)
+    #         end_time = datetime.max.time()
+    #         end_date = datetime.combine(rec.date_to, end_time)         
+    #         po_lines = self.env['purchase.order.line'].search([
+    #             ('account_analytic_id','=', rec.analytic_account_id.id),
+    #             ('order_id.state','in', ['draft','sent','to approve', 'to reapprove']),
+    #             ('date_promised', '>=', start_date ),
+    #             ('date_promised', '<=', end_date )
+    #         ])
+    #         total_burden = 0.0
+    #         for po_line in po_lines:
+    #             if po_line.product_qty != 0:
+    #                 units = (po_line.product_qty - po_line.qty_invoiced)
+    #                 total_burden += po_line.price_unit * units
+    #         rec.draft_burden = total_burden
 
 
 
